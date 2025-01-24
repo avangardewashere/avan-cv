2611c6547a62393d02ddb17184b89ba80d96936c
avangardewashere <avelpanaligan@gmail.com>
2025-01-24 21:53:05 +0800

    add/ruting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -5,7 +6,9 @@ const Navbar = () => {
    <div className="bg-background h-[60px] w-full relative">
     <nav className="text-sm">
         <ul className='decoration-none'>
-          
+          <li>Home</li>
+          <li>About Me</li>
+          <li><Link href={"/projects"}>Projects</Link></li>
         </ul>
     </nav>
     <div className='absolute h-[4px] bg-lineargold w-full bottom-0'/> 
