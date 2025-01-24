f1610806e4bc8994373ee4cb7996911bb1c6e4eb
avangardewashere <avelpanaligan@gmail.com>
2025-01-25 03:21:00 +0800

    add/color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-   <div className="bg-background h-[60px] w-full relative">
-    <nav className="text-sm">
-        <ul className='decoration-none'>
-          <li>Home</li>
-          <li>About Me</li>
-          <li><Link href={"/projects"}>Projects</Link></li>
+   <div className="bg-background h-[60px] w-full relative flex  items-center justify-between  text-[#222]">
+    <div className="logo rounded-full bg-sky-500 w-8 h-8 ml-10"></div>
+    <nav className="text-sm  mr-10" >
+        <ul className='flex space-x-3 decoration-none'>
+          <li className='hover:text-blue-400'>Home</li>
+          <li className='hover:text-blue-400'>About Me</li>
+          <li className='hover:text-blue-400'><Link href={"/projects"}>Projects</Link></li>
         </ul>
     </nav>
-    <div className='absolute h-[4px] bg-lineargold w-full bottom-0'/> 
+    <div className='absolute h-[4px] bg-lineargold w-full bottom-0 '/> 
    </div>
   )
 }
