8e94c2ce0a649f8f67f08f1ddd11bee6b2ca7721
avangardewashere <avelpanaligan@gmail.com>
2025-01-26 02:13:37 +0800

    save/unsave-changes

--- components/Heroes.tsx
+++ components/Heroes.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Heroes = () => {
   return (
-    <div className="p-6 sm:p-10 w-fit">
+    <div className="p-6 sm:p-8 w-fit">
       <span className="text-sm/6 sm:text-lg">Hello Welcome, My name is</span>
       <h1 className="text-sky-600 font-bold text-4xl sm:text-5xl/ ">
         Avel Panaligan
--- components/KnowMe.tsx
+++ components/KnowMe.tsx
@@ -3,20 +3,28 @@ import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 
 const KnowMe = () => {
   return (
-    <div>
-      <h2 className="text-sky-600">Get To Know me</h2>
+    <div className="flex flex-col p-6 sm:p-8">
+      <h2 className="text-sky-600 text-lg text-semibold sm:mx-0 mx-auto">Get To Know me</h2>
       <Carousel
         opts={{
           align: "start",
+          loop:true,
+          duration:3000,
+        
         }}
         orientation="vertical"
-        className="w-full max-w-xs"
+        className="w-[300px] sm:w-[250px] h-[150px]  sm:h-[100px]    max-w-xs mx-auto overflow-hidden"
         
       >
-        <CarouselContent className="-mt-1 h-[200px]">
-          <CarouselItem className="pt-1 md:basis-1/2">Hello</CarouselItem>
-          <CarouselItem className="pt-1 md:basis-1/2">Hi</CarouselItem>
-          <CarouselItem className="pt-1 md:basis-1/2">Hello</CarouselItem>
+        <CarouselContent className="mt-1 p-2  bg-sky-300/20 space-y-2 ">
+          <CarouselItem className="pt-1 md:basis-1/2 basis-1/3  bg-slate-300 rounded-md">
+          meow
+          </CarouselItem>
+          <CarouselItem className="pt-1 md:basis-1/2 basis-1/3  bg-slate-300 rounded-md">Hi</CarouselItem>
+          <CarouselItem className="pt-1 md:basis-1/2 basis-1/3  bg-slate-300 rounded-md">Hello</CarouselItem>
+          <CarouselItem className="pt-1 md:basis-1/2 basis-1/3  bg-slate-300 rounded-md">thank you</CarouselItem>
+          <CarouselItem className="pt-1 md:basis-1/2 basis-1/3  bg-slate-300 rounded-md">goodbye</CarouselItem>
+          <CarouselItem className="pt-1 md:basis-1/2 basis-1/3  bg-slate-300 rounded-md">scrap</CarouselItem>
         </CarouselContent>
       </Carousel>
     </div>
