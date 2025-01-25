adec26f0c4f19117c7475bd84fab3e022c8ac076
avangardewashere <avelpanaligan@gmail.com>
2025-01-26 02:19:45 +0800

    add/skills-section

--- components/Heroes.tsx
+++ components/Heroes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Heroes = () => {
   return (
+    <>
     <div className="p-6 sm:p-8 w-fit">
       <span className="text-sm/6 sm:text-lg">Hello Welcome, My name is</span>
       <h1 className="text-sky-600 font-bold text-4xl sm:text-5xl/ ">
@@ -11,6 +12,13 @@ const Heroes = () => {
         I create <span className="text-sky-600">Web Site</span>
       </span>
     </div>
+    <div className="px-6 flex space-x-2 ">
+      <div className="w-10 h-10 bg-sky-600 rounded-full"></div>
+      <div className="w-10 h-10 bg-sky-600 rounded-full"></div>
+      <div className="w-10 h-10 bg-sky-600 rounded-full"></div>
+      <div className="w-10 h-10 bg-sky-600 rounded-full"></div>
+    </div>
+    </>
   );
 };
 
