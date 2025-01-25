b5612a22b143c5596d22809a765597d51b750873
avangardewashere <avelpanaligan@gmail.com>
2025-01-26 02:24:35 +0800

    images

--- components/ProjectsCarousel.tsx
+++ components/ProjectsCarousel.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const ProjectsCarousel = () => {
   return (
     <div>
-      Projects
+     <Image src={"/images/foodie-pal.png"} height={150} width={250} alt="foodie-pal" className='object-cover'/> 
     </div>
   )
 }
