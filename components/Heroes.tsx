5e6a35660066de4dc83cd8a0ef402e7b832eb204
avangardewashere <avelpanaligan@gmail.com>
2025-01-25 19:37:07 +0800

    add/carousel

--- components/Heroes.tsx
+++ components/Heroes.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Heroes = () => {
   return (
-    <div className="p-6 sm:p-10">
-      <span className="text-sm sm:text-lg">Hello Welcome, My name is</span>
-      <h1 className="text-sky-600 font-bold text-4xl sm:text-5xl ">
+    <div className="p-6 sm:p-10 w-fit">
+      <span className="text-sm/6 sm:text-lg">Hello Welcome, My name is</span>
+      <h1 className="text-sky-600 font-bold text-4xl sm:text-5xl/ ">
         Avel Panaligan
       </h1>
+      <span className="text-sm/1 sm:text-lg">I create <span className="text-sky-600">Web Site</span></span>
     </div>
   );
 };
