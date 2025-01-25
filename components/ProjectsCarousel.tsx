866eec3a227ce48000fb0422e147e0ea86c552f6
avangardewashere <avelpanaligan@gmail.com>
2025-01-26 02:39:24 +0800

    update/project-carousel

--- components/ProjectsCarousel.tsx
+++ components/ProjectsCarousel.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ProjectsCarousel = () => {
   return (
     <div className='flex items-center justify-center w-full'>
-     <Image src={"/images/foodie-pal.png"} height={180} width={280} alt="foodie-pal" className='object-cover rounded-md border border-t-4 border-amber-500 '/> 
+     <Image src={"/images/foodie-pal.png"} height={180} width={280} alt="foodie-pal" className='object-cover rounded-md border-4 border-sky-500 '/> 
     </div>
   )
 }
