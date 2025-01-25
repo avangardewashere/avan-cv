b263c50d497fed4b802f29dc47623d3d8d73bc3c
avangardewashere <avelpanaligan@gmail.com>
2025-01-25 19:31:52 +0800

    add/heroes-heading

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,10 @@
+import Heroes from "@/components/Heroes";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="h-[100vh] bg-blue-100">
-    <span>move to group</span>
+   <div className=" ">
+    <Heroes /> 
    </div>
   );
 }
