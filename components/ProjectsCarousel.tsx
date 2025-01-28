4c3f475db95cb3f202d03e2e2d9ea861a9a6fed8
avangardewashere <avelpanaligan@gmail.com>
2025-01-28 18:11:09 +0800

    add/image-styles

--- components/ProjectsCarousel.tsx
+++ components/ProjectsCarousel.tsx
@@ -1,12 +1,19 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const ProjectsCarousel = () => {
   return (
-    <div className='flex items-center justify-center w-full'>
-     <Image src={"/images/foodie-pal.png"} height={180} width={280} alt="foodie-pal" className='object-cover rounded-md border-4 border-sky-500 '/> 
+    <div className="flex items-center justify-center w-full h-100">
+      <div className="relative w-[360px] h-[240px]">
+        <Image
+          src={"/images/foodie-pal.png"}
+          fill
+          alt="foodie-pal"
+          className="  object-cover rounded-md border-4 border-sky-500 "
+        />{" "}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectsCarousel
+export default ProjectsCarousel;
