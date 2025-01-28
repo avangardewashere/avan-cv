92b731c048115dbbc732c9210b2e2866f2f79e95
avangardewashere <avelpanaligan@gmail.com>
2025-01-28 10:21:38 +0800

    add/skill-data-type

--- data/index.ts
+++ data/index.ts
@@ -1,29 +1,76 @@
-import { NavigationDataType } from "./types";
+import { NavigationDataType, SkillDataIconsType } from "./types";
+import ReactIcon from "@/public/images/icons/reactjs.png"
+import NextIcon from "@/public/images/icons/reactjs.png"
+import TailwindIcon from "@/public/images/icons/reactjs.png"
+import WpIcon from "@/public/images/icons/reactjs.png"
+export const NavigationData: NavigationDataType[] = [
+  {
+    id: 1,
+    name: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    name: "About Me",
+    href: "/about",
+  },
+  {
+    id: 3,
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    id: 4,
+    name: "Skills",
+    href: "/skills",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    href: "/contact",
+  },
+];
 
-export const NavigationData:NavigationDataType[] = [
-    {
-      id: 1,
-      name: "Home",
-      href: "/",
-    },
-    {
-      id: 2,
-      name: "About Me",
-      href: "/about",
-    },
-    {
-      id: 3,
-      name: "Projects",
-      href: "/projects",
-    },
-    {
-      id: 4,
-      name: "Skills",
-      href: "/skills",
-    },
-    {
-      id: 5,
-      name: "Contact",
-      href: "/contact",
-    },
-  ];
+export const SkillData: SkillDataIconsType[] = [
+  {
+    name: "React",
+    href: "",
+    active: false,
+    design: "",
+    src: ReactIcon,
+    id: 11,
+  },
+  {
+    name: "Next",
+    href: "",
+    active: false,
+    design: "",
+    src: NextIcon,
+    id: 11,
+  },
+  {
+    name: "Next",
+    href: "",
+    active: false,
+    design: "",
+    src: NextIcon,
+    id: 11,
+  },
+
+  {
+    name: "Wordpress",
+    href: "",
+    active: false,
+    design: "",
+    src: WpIcon,
+    id: 13,
+  },
+  {
+    name: "TailwindCss",
+    href: "",
+    active: false,
+    design: "",
+    src: TailwindIcon,
+    id: 14,
+  },
+];
--- data/types.ts
+++ data/types.ts
@@ -1,5 +1,16 @@
+import { StaticImageData } from "next/image";
+
 export type NavigationDataType = {
   name: string;
   href: string;
   id: number;
 };
+
+export type SkillDataIconsType ={
+  name:string,
+  id:number,
+  href:string,
+  active?:boolean,
+  src:string | StaticImageData,
+  design?:string
+}
