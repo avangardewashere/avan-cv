00dcb1b0691d3aa6d37c2b07ea1f6973a3f39539
avangardewashere <avelpanaligan@gmail.com>
2025-01-28 11:06:49 +0800

    add/know-me-card

---
+++ components/cards/KnowMeCard.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+const KnowMeCard = () => {
+  return (
+    <div className="pt-1 min-h-12  bg-slate-300 rounded-md">
+      <div></div>
+      <Link href="/">
+        <div className="cardContent">
+          {/* logo */}
+          {/* name */}
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default KnowMeCard;
