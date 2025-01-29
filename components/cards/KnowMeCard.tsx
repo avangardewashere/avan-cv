753775bdd88921b4ef0e6914e303bef95f2059b3
avangardewashere <avelpanaligan@gmail.com>
2025-01-29 16:37:11 +0800

    add/know-me-card-design

--- components/cards/KnowMeCard.tsx
+++ components/cards/KnowMeCard.tsx
@@ -2,18 +2,28 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+
+
 const KnowMeCard = () => {
   return (
     <>
-    <Link href="/">
-      <div className="pt-1 min-h-12 border flex space-x-2  items-center justify-start pl-4 border-amber-200 bg-slate-300 rounded-md">
-        <Image src={"/images/icons/nextjs.png"} alt="" width={20} height={20} />
-        <div className="cardContent">
-          {/* logo */}
-          <span className=" font-semibold "> Visit my Github Profile</span>
+      <Link href="/">
+        <div
+          style={{ background: "linear-gradient(120deg,#6D8AE3,#427DCA)" }}
+          className="pt-1 min-h-12 border flex space-x-2  items-center justify-start pl-4 border-amber-200 bg-slate-300 rounded-lg"
+        >
+          <Image
+            src={"/images/icons/github.png"}
+            alt={"image"}
+            width={20}
+            height={20}
+            // style={{filter:"brightness(1000%)"}}
+          />
+          <div className="cardContent">
+            <span className=" font-semibold text-white"> Visit my Github Profile</span>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
     </>
   );
 };
