db0f7ce49633a644ef3ae6c9efd078523692bf2a
avangardewashere <avelpanaligan@gmail.com>
2025-01-29 14:11:00 +0800

    fix/heroes

--- components/Heroes.tsx
+++ components/Heroes.tsx
@@ -5,15 +5,16 @@ import React from "react";
 const Heroes = () => {
   return (
     <div className="flex flex-col">
-      <div className="px-6 py-4 pb-3  sm:p-8 w-fit md:px-12 ">
+      <div className="px-6 py-4 pb-3  sm:p-8  md:px-12 w-fit sm:w-[600px]">
         <span className="text-[14]/3 sm:text-lg md:text-2xl">
           Hello Welcome, My name is
         </span>
         <h1 className="text-sky-600 font-bold text-[42px]/10 sm:text-5xl md:text-6xl ">
           Avel Panaligan
         </h1>
-        <span className="text-[14]/3 sm:text-lg md:text-2xl">
-          I create <span className="text-sky-600">Web Site</span>
+        <span className="text-[14]/3 sm:text-lg md:text-2xl ">
+          I create <span className="text-sky-600 mr-2">Websites</span>
+          using different frameworks and various libraries which some of are
         </span>
       </div>
       <div className="px-6 py-[.5px] flex space-x-3 sm:px-8  md:px-12 ">
