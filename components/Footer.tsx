636e6ce22bdb0b6f4192238596ebd3ad4eda927a
avangardewashere <avelpanaligan@gmail.com>
2025-02-01 23:38:58 +0800

    udpate/components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import QuickLinks from './overview/QuickLinks'
 
 const Footer = () => {
   return (
     <div className='bg-sky-100 min-h-[400px] '>
       <span>Footer</span>
+      <QuickLinks/>
     </div>
   )
 }
--- components/overview/Projects.tsx
+++ components/overview/Projects.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import ProjectCards from "../cards/ProjectCards";
 
 const Projects = () => {
   return (
     <div>
       <span>Project section</span>
+      <span>
+        <ProjectCards />
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
