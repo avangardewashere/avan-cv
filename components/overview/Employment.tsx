e324431413e50d0753f1e0489c3721ec89f2f617
avangardewashere <avelpanaligan@gmail.com>
2025-02-02 18:26:40 +0800

    add/employment-container

--- components/overview/Employment.tsx
+++ components/overview/Employment.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Employment = () => {
   return (
-    <div className='flex'>
-      <div>
-        <div className='flex border items-center justify-center '>
-            <div className='font-semibold'>Front End Developer</div>
+    <div className="flex">
+      <div className="flex flex-row">
 
+        <div className="flex border items-center justify-center ">
+          <div className="font-semibold text-base">Front End Dev</div>
+          <div className="text-sm text-slate-500">(2024-2025)</div>
         </div>
-      </div>
-      <div><span>employemnet</span></div>
-    </div>
-  )
-}
 
-export default Employment
+        <div className="flex border items-center justify-center ">
+          <div className="font-semibold text-base">Front End Dev</div>
+          <div className="text-sm text-slate-500">(2024-2025)</div>
+        </div>
+        
+      </div>
+      <div>
+        <span>employemnet</span>
+      </div>
+    </div>
+  );
+};
+
+export default Employment;
