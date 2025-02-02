b866ab5eebfc7414d43de54edc0181ad24599498
avangardewashere <avelpanaligan@gmail.com>
2025-02-02 18:13:35 +0800

    add card

--- components/cards/ProjectCards.tsx
+++ components/cards/ProjectCards.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProjectCards = () => {
   return (
-    <div className='flex w-[150px] h-20 border rounded-lg justify-center align-center'>
+    <div className='flex w-[150px] h-20 border rounded-lg justify-center items-center'>
       <span>Project Card</span>
     </div>
   )
