d7e0aaa9f168f1ba666b0c3b15fdc3ec5419fbaa
avangardewashere <avelpanaligan@gmail.com>
2025-02-02 19:01:55 +0800

    add/employment-container

--- components/overview/Employment.tsx
+++ components/overview/Employment.tsx
@@ -4,7 +4,6 @@ const Employment = () => {
   return (
     <div className="flex">
       <div className="flex flex-row">
-
         <div className="flex border items-center justify-center ">
           <div className="font-semibold text-base">Front End Dev</div>
           <div className="text-sm text-slate-500">(2024-2025)</div>
@@ -14,11 +13,21 @@ const Employment = () => {
           <div className="font-semibold text-base">Front End Dev</div>
           <div className="text-sm text-slate-500">(2024-2025)</div>
         </div>
-
       </div>
       <div>
-        <div className="w-[70%] h-[200px]">
-            
+        <div className="flex flex-row w-[70%] h-[200px]">
+          <div className="flex">
+            <div>
+              <div>Comapny Name</div>
+              <div>Positon</div>
+              <div>year</div>
+            </div>
+          </div>
+          <div>
+            <div>Comapny Name</div>
+            <div>Positon</div>
+            <div>year</div>
+          </div>
         </div>
       </div>
     </div>
