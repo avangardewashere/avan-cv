5ed65ab5bd1376e44aa2da99d5aca82362272231
avangardewashere <avelpanaligan@gmail.com>
2025-02-03 23:24:16 +0800

    made/changes

--- components/cards/SkillCard.tsx
+++ components/cards/SkillCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SkillCard = () => {
   return (
-    <div className='flex border w-64 h-20 bg-sky-50'>
+    <div className='flex border w-64 h-20 bg-sky-100 '>
       <span>Skill card</span>
     </div>
   )
